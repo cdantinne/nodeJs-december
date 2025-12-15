613fa1af93b24946c731f02725b0741341251fbe
Corentin Dantinne <dantinne.corentin@gmail.com>
2025-12-15 12:57:14 +0100

    asyncHandler

--- Exercice_1/src/utils/asyncHandler.js
+++ Exercice_1/src/utils/asyncHandler.js
@@ -1,6 +1,15 @@
-// exports.asyncHandler = (f) => {
-//     new Promise((resolve, reject)=>{
-       
-//     })
+exports.asyncHandler = (f) => {
+    return(
+        function (...args) {
+            return new Promise ((resolve, reject)=>{
+                try{
+                    const result = f(...args)
+                    resolve(result)
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        }
+    )
 
-// }
+}
